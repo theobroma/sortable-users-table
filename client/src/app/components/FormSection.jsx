a6c983fc6acae3bb9eeba0320b79fecfd3e3f599
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-19 02:42:27 +0300

    onChange form

--- client/src/app/components/FormSection.jsx
+++ client/src/app/components/FormSection.jsx
@@ -9,12 +9,28 @@ const options = [
 ];
 
 class FormSection extends React.Component {
-  state = {};
+  state = {
+    first_name: 'Hello',
+    last_name: '',
+    phone1: 555,
+    phone2: 555,
+    phone3: 9999,
+    gender: 'female',
+    age: 0
+  };
 
-  handleChange = (e, { value }) => this.setState({ value });
+  onChange = (e, data) => {
+    //console.log(e.target);
+    //console.log(data);
+    const name = e.target.name || data.name;
+    const value = e.target.value || data.value;
+    this.setState({ [name]: value }, () => {
+      console.log(this.state);
+    });
+  };
 
   render() {
-    const { value } = this.state;
+    const { first_name, last_name, phone1, phone2, phone3, gender, age } = this.state;
 
     const formHeader = (
       <Header as="h2" color="teal" textAlign="center">
@@ -28,31 +44,67 @@ class FormSection extends React.Component {
         <Segment stacked raised>
           <Form.Field>
             <label>First Name</label>
-            <input placeholder="First Name" />
+            <input
+              placeholder="First Name"
+              value={first_name}
+              name="first_name"
+              onChange={this.onChange}
+            />
           </Form.Field>
           <Form.Field>
             <label>Last Name</label>
-            <input placeholder="Last Name" />
+            <input
+              placeholder="Last Name"
+              value={last_name}
+              name="last_name"
+              onChange={this.onChange}
+            />
           </Form.Field>
           <Form.Field>
             <label>Age</label>
-            <input placeholder="Age" />
+            <input placeholder="Age" value={age} name="age" onChange={this.onChange} />
           </Form.Field>
           <Form.Field>
-            <Form.Select fluid label="Gender" options={options} placeholder="Gender" />
+            <Form.Select
+              fluid
+              label="Gender"
+              options={options}
+              placeholder="Gender"
+              value={gender}
+              name="gender"
+              onChange={this.onChange}
+            />
           </Form.Field>
           <Form.Group widths="equal">
             <Form.Field>
               <label style={{ visibility: 'hidden' }}>Phone Number</label>
-              <Input placeholder="(xxx)" size="mini" />
+              <Input
+                placeholder="(xxx)"
+                size="mini"
+                value={phone1}
+                name="phone1"
+                onChange={this.onChange}
+              />
             </Form.Field>
             <Form.Field>
               <label>Phone Number</label>
-              <Input placeholder="xxx" size="mini" />
+              <Input
+                placeholder="xxx"
+                size="mini"
+                value={phone2}
+                name="phone2"
+                onChange={this.onChange}
+              />
             </Form.Field>
             <Form.Field>
               <label style={{ visibility: 'hidden' }}>Phone Number</label>
-              <Input placeholder="xxxx" size="mini" />
+              <Input
+                placeholder="xxxx"
+                size="mini"
+                value={phone3}
+                name="phone3"
+                onChange={this.onChange}
+              />
             </Form.Field>
           </Form.Group>
           <Form.Button color="teal" fluid size="tiny">
