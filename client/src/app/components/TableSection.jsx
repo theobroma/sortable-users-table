36f1b2b9cf435b82ee1b6a771d77029b04993f27
Aleksandr Siryi <theobroma222@gmail.com>
2018-06-21 03:41:24 +0300

    handleSort

--- client/src/app/components/TableSection.jsx
+++ client/src/app/components/TableSection.jsx
@@ -1,49 +1,83 @@
 import React from 'react';
 import cx from 'classnames';
+import orderBy from 'lodash/orderBy';
 import { Grid, Table, Icon } from 'semantic-ui-react';
 //mock data if needed
 import MOCK_DATA from '../helpers/MOCK_DATA.json';
 //import TableRow from './TableRow';
 import TableRow from '../Containers/TableRow';
 
-export default class MainSection extends React.Component {
+const invertDirection = {
+  asc: 'desc',
+  desc: 'asc'
+};
+
+export default class TableSection extends React.Component {
+  state = {
+    data: [],
+    columnToSort: '',
+    sortDirection: 'desc'
+  };
+
   renderUsers() {
     const { users } = this.props;
     return users.map(user => <TableRow key={user.id} user={user} />);
   }
 
-  // [{
-  // 	"id": "d80adb99-4b36-41d4-920f-b7d54b33fd54",
-  // 	"firstName": "Kristan",
-  // 	"lastName": "Duthie",
-  // 	"phone": "693-261-4129",
-  // 	"gender": "Female",
-  // 	"age": 64
-  // }, {
+  handleSort = columnName => {
+    this.setState(
+      state => ({
+        columnToSort: columnName,
+        sortDirection:
+          state.columnToSort === columnName ? invertDirection[state.sortDirection] : 'asc'
+      }),
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
 
   render() {
+    const header = [
+      {
+        name: 'First name',
+        prop: 'firstName'
+      },
+      {
+        name: 'Last name',
+        prop: 'lastName'
+      },
+      {
+        name: 'Phone',
+        prop: 'phone'
+      },
+      {
+        name: 'Gender',
+        prop: 'gender'
+      },
+      {
+        name: 'Age',
+        prop: 'age'
+      }
+    ];
+
     const tableHeader = (
       <Table.Header>
         <Table.Row>
-          <Table.HeaderCell>
-            First Name<Icon name={'sort'} />
-          </Table.HeaderCell>
-          <Table.HeaderCell>
-            Last Name<Icon name={'sort'} />
-          </Table.HeaderCell>
-          <Table.HeaderCell>
-            Phone<Icon name={'sort'} />
-          </Table.HeaderCell>
-          <Table.HeaderCell>
-            gender<Icon name={'sort'} />
-          </Table.HeaderCell>
-          <Table.HeaderCell>
-            age<Icon name={'sort'} />
-          </Table.HeaderCell>
+          {header.map((item, idx) => {
+            return (
+              <Table.HeaderCell key={`thc-${idx}`} onClick={() => this.handleSort(item.prop)}>
+                {item.name}
+                <Icon name={'sort'} />
+              </Table.HeaderCell>
+            );
+          })}
+          {/*for delete btn*/}
           <Table.HeaderCell />
         </Table.Row>
       </Table.Header>
     );
+
     return (
       <Grid textAlign="center" verticalAlign="middle" className="register-form-inner">
         <Grid.Row>
@@ -58,3 +92,34 @@ export default class MainSection extends React.Component {
     );
   }
 }
+
+// const tableHeader = (
+//   <Table.Header>
+//     <Table.Row>
+//       <Table.HeaderCell onClick={() => this.handleSort()}>
+//         First Name<Icon name={'sort'} />
+//       </Table.HeaderCell>
+//       <Table.HeaderCell onClick={() => this.handleSort()}>
+//         Last Name<Icon name={'sort'} />
+//       </Table.HeaderCell>
+//       <Table.HeaderCell onClick={() => this.handleSort()}>
+//         Phone<Icon name={'sort'} />
+//       </Table.HeaderCell>
+//       <Table.HeaderCell onClick={() => this.handleSort()}>
+//         gender<Icon name={'sort'} />
+//       </Table.HeaderCell>
+//       <Table.HeaderCell onClick={() => this.handleSort()}>
+//         age<Icon name={'sort'} />
+//       </Table.HeaderCell>
+//     </Table.Row>
+//   </Table.Header>
+// );
+
+// [{
+// 	"id": "d80adb99-4b36-41d4-920f-b7d54b33fd54",
+// 	"firstName": "Kristan",
+// 	"lastName": "Duthie",
+// 	"phone": "693-261-4129",
+// 	"gender": "Female",
+// 	"age": 64
+// }, {
