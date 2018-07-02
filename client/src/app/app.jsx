4ee97e5febb6d73707c9f7cf479513a678530776
Aleksandr Siryi <theobroma222@gmail.com>
2018-07-02 15:50:51 +0300

    upd webpack

--- client/src/app/app.jsx
+++ client/src/app/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import './styles/index.scss';
+import './assets/styles/index.scss';
 import MainApp from './MainApp';
 import configureStore from './configureStore';
 
